1ed4383ba7868ef74a5a1d864901dddfd06fd4ce
RGNew <rn1076@gmail.com>
2018-12-01 21:50:11 -0600

    Created function for generating random word

--- random_stuff.py
+++ random_stuff.py
@@ -13,7 +13,19 @@ rand_val = random.randint(0,25)
 
 word_length = 5
 
-for i in range(word_length):
-     rand_val = random.randint(0,25)  # select a random int from 0 to 25
-     letter = alphabet[rand_val]  # use previous random int to select letter
-     print(letter, end='')  # print the letter with no newline
+def random_word_gen(word_length=5):
+    '''
+    Returns a random word of word_length composed of letters of the alphabet.
+    '''
+    
+    word = ''
+
+    for i in range(word_length):
+        rand_val = random.randint(0,25)  # select a random int from 0 to 25
+        letter = alphabet[rand_val]  # use previous random int to select letter
+        word += letter  #  add each letter together into a string
+
+    return word
+
+
+print(f'The word is {random_word_gen(word_length)}.')  # print out the full word
