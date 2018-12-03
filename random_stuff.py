0213f540acb2da2f6fade5b89099d4fa63059f8e
RGNew <rn1076@gmail.com>
2018-12-03 13:24:07 -0600

    Added function to generate sentences

--- random_stuff.py
+++ random_stuff.py
@@ -13,6 +13,8 @@ rand_val = random.randint(0,25)
 
 word_length = 5
 
+sentence_word_count = 5
+
 def random_word_gen(word_length=5):
     '''
     Returns a random word of word_length composed of letters of the alphabet.
@@ -27,4 +29,19 @@ def random_word_gen(word_length=5):
 
     return word
 
-print(f'The word is {random_word_gen(word_length)}.')  # print out the full word
+def random_sentence_gen(sentence_word_count=5):
+    '''
+    Returns a sentence of sentence_word_count composed of randomly generated words.
+    It uses the random_word_gen function to generate words.
+    '''
+
+    sentence = ''
+
+    for _ in range(sentence_word_count):  # for loop used to create the sentence by concatinating random words
+        rand_word = random_word_gen()  # calls the random_word_gen function to generate random word
+        sentence = sentence + ' ' + rand_word  # combines the randomly generated words into a sentence
+    
+    return sentence
+
+#print(f'The word is {random_word_gen(word_length)}.')  # print out the full word
+print(random_sentence_gen(sentence_word_count))  # prints the sentence to the console
